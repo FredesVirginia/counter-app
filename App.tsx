51252b7e3e2694e6af9683611afd72d7e3c0a334
Fredes Virginia <William-Champion@outlook.com>
2025-07-25 03:39:36 -0300

    w

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHuge}>Hola Jarry</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+         onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: "white", fontSize: 25 }}>Pip + 1</Text>{" "}
+      </Pressable>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,8 +23,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textHuge: {
+    fontSize: 60,
+    fontWeight: "500",
+  },
+
+  floatingButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#65558F",
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 3,
   },
 });
